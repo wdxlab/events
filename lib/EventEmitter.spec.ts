ab5381c028a3766e8a872d5dab8fb18ef26b91ff
Sergey Melyukov <s.melukov@gmail.com>
2020-07-22 16:13:33 +0300

    test: add more tests

--- lib/EventEmitter.spec.ts
+++ lib/EventEmitter.spec.ts
@@ -6,14 +6,62 @@ class Stub {
 
 type EventArg = { foo: string };
 
-test('should emit', () => {
+test('add the listeners and emit', () => {
   const stub = new Stub();
   const eventArg: EventArg = { foo: 'hello' };
   const event = new EventEmitter<Stub, EventArg>();
-  event.on((sender, arg) => {
-    expect(sender).toBe(stub);
-    expect(arg).toStrictEqual(arg);
-  });
+  const handler = jest.fn();
+  const handler2 = jest.fn();
 
+  event.on(handler);
+  event.on(handler);
+  event.on(handler2);
+  expect(event.subscriptionsCount).toBe(2);
   event.emit(stub, eventArg);
+
+  expect(handler).toBeCalledTimes(1);
+  expect(handler).toBeCalledWith(stub, eventArg);
+  expect(handler2).toBeCalledTimes(1);
+  expect(handler2).toBeCalledWith(stub, eventArg);
+});
+
+test('remove listeners', () => {
+  const stub = new Stub();
+  const eventArg: EventArg = { foo: 'hello' };
+  const event = new EventEmitter<Stub, EventArg>();
+  const handler = jest.fn();
+  const handler2 = jest.fn();
+
+  event.on(handler);
+  event.on(handler2);
+  expect(event.subscriptionsCount).toBe(2);
+  event.emit(stub, eventArg);
+  event.off(handler);
+  expect(event.subscriptionsCount).toBe(1);
+  event.emit(stub, eventArg);
+
+  expect(handler).toBeCalledTimes(1);
+  expect(handler).toBeCalledWith(stub, eventArg);
+  expect(handler2).toBeCalledTimes(2);
+  expect(handler2).toBeCalledWith(stub, eventArg);
+});
+
+test('clear listeners', () => {
+  const stub = new Stub();
+  const eventArg: EventArg = { foo: 'hello' };
+  const event = new EventEmitter<Stub, EventArg>();
+  const handler = jest.fn();
+  const handler2 = jest.fn();
+
+  event.on(handler);
+  event.on(handler2);
+  event.emit(stub, eventArg);
+  event.clear();
+  expect(event.subscriptionsCount).toBe(0);
+  event.emit(stub, eventArg);
+
+  expect(handler).toBeCalledTimes(1);
+  expect(handler).toBeCalledWith(stub, eventArg);
+  expect(handler2).toBeCalledTimes(1);
+  expect(handler2).toBeCalledWith(stub, eventArg);
 });
